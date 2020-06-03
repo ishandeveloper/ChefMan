25f2448f61b14003d218a90c8b14e729a73f48e1
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-03 19:53:53 +0530

    Secret Keys

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
 
 function App() {
+
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
     </div>
-  );
+  )
 }
+
 
 export default App;
